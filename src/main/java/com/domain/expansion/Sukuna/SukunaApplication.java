4624c3bd03086c11d3378a9a0e37954de1753ba6
Man kit Ho <mankitho@Mans-MBP.lan>
2024-05-18 19:01:54 -0400

    init

---
+++ src/main/java/com/domain/expansion/Sukuna/SukunaApplication.java
@@ -0,0 +1,13 @@
+package com.domain.expansion.Sukuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SukunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SukunaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domain/expansion/Sukuna/SukunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.expansion.Sukuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SukunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
